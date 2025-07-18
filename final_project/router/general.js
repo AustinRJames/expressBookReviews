300901c3b3f03560bb2bbeff1a52e383f19e7f40
AustinRJames <arjames1128@gmail.com>
2025-07-18 17:20:47 -0400

    Up to task 5

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,31 +13,65 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  res.send(JSON.stringify(books, null, 4));
+  
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
+    let num = parseInt(req.params.isbn)
+    if (num < 1 || num > 10) {
+        // Send error message
+        res.send("Select a correct isbn");
+    } else {
+        res.send("Book Searched: " + JSON.stringify(books[num], null, 4));
+    }
+
+ }); 
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+    const authorName = req.params.author;
+    const result = {};
+    
+    // Iterate through books object
+    Object.keys(books).forEach(isbn => {
+      if (books[isbn].author.toLowerCase() === authorName.toLowerCase()) {
+        result[isbn] = books[isbn];
+      }
+    });
+    
+    if (Object.keys(result).length === 0) {
+      return res.status(404).json({message: "No books found for this author"});
+    }
+    
+    res.json(result);
+  });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const titleName = req.params.title;
+    
+    const result = {};
+
+    // Iterate through books 
+    Object.keys(books).forEach(isbn => {
+        if (books[isbn].title.toLowerCase().replaceAll(" ", "") === titleName.toLocaleLowerCase()) {
+            result[isbn] = books[isbn];
+        }
+    });
+
+    if (Object.keys(result).length === 0) {
+        return res.status(404).json({message: "No Books found by this title"});
+    }
+
+    res.json(result);
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    let isbn = parseInt(req.params.isbn);
+    res.send(books[isbn].reviews);
 });
 
 module.exports.general = public_users;
